c542234433598b54f28ef760c6e4d25f8a82a730
charlessilva1983 <charles.silva1983@gmail.com>
2018-08-04 10:33:51 +0200

    change param directory name to reflect a generic datafile directory name.

--- variables.py
+++ variables.py
@@ -1,2 +1,2 @@
 # define the pattern to the source file directory where the datafile is available
-default_directory = 'uploads/datasetname_{}-{}-{}'
+default_directory = 'datafiles/datasetname_{}-{}-{}'
